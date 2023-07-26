8c5db6004b45fd19f34888f06e9459a2bcfac54b
Felipe Tuyama <11530478+ftuyama@users.noreply.github.com>
2023-07-26 19:39:02 -0300

    Dynamic students + Remove template pics

--- js/googlesheets.js
+++ js/googlesheets.js
@@ -3,10 +3,60 @@ function readGoogleSheet(tabName, callback) {
   var apiKey = "AIzaSyAsSKLcTCFgVfIrgYD4bJAG-evsZQ-wRF8";
   var url = `https://sheets.googleapis.com/v4/spreadsheets/${spreadsheetID}/values/${tabName}?alt=json&key=${apiKey}`;
 
-  $.getJSON(url, function(data) {
+  $.getJSON(url, function (data) {
     data.values.shift();  // Remove header
     callback(data.values);
   });
+}
+
+function alunosCarousel() {
+  // Testimonials carousel
+  $(".alunos-carousel").owlCarousel({
+    center: true,
+    autoplay: true,
+    smartSpeed: 2000,
+    dots: true,
+    loop: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      576: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      992: {
+        items: 3
+      }
+    }
+  });
+}
+
+function addAlunos(alunos) {
+  alunos.forEach((aluno, index) => {
+    const alunoHTML = `
+      <div class="testimonial-item px-3">
+        <div class="bg-light shadow-sm rounded mb-4 p-4">
+          <h3 class="fas fa-quote-left text-primary mr-3"></h3>
+          ${aluno[2]}
+        </div>
+        <div class="d-flex align-items-center">
+          <img class="rounded-circle" src="img/apajac/2012_2.jpg"
+            style="width: 70px; height: 70px;" alt="Image">
+          <div class="pl-3">
+            <h5>${aluno[0]}</h5>
+            <i>${aluno[1]}</i>
+          </div>
+        </div>
+      </div>
+    `;
+
+    $('.alunos').append(alunoHTML);
+  });
+
+  alunosCarousel();
 }
 
 function addEventos(eventos) {
@@ -14,7 +64,7 @@ function addEventos(eventos) {
     const eventoHTML = `
         <div class="col-lg-4 mb-5">
         <div class="card border-0 bg-light shadow-sm pb-2">
-          <img class="card-img-top mb-2" src="img/class-${index + 1}.jpg" alt>
+          <img class="card-img-top mb-2" src="img/imageKid.png" alt>
           <div class="card-body text-center">
             <h4 class="card-title">${evento[0]}</h4>
             <p class="card-text">${evento[1]}</p>
@@ -60,7 +110,7 @@ function addVoluntarios(voluntarios) {
       <div class="col-md-6 col-lg-3 text-center team mb-5">
         <div class="position-relative overflow-hidden mb-4"
           style="border-radius: 100%;">
-          <img class="img-fluid w-100" src="img/team-${index + 1}.jpg" alt>
+          <img class="img-fluid w-100" src="img/apajac/2012_2.jpg" alt>
           <div
             class="team-social d-flex align-items-center justify-content-center w-100 h-100 position-absolute">
             <a class="btn btn-outline-light text-center mr-2 px-0"
@@ -93,4 +143,5 @@ function addVoluntarios(voluntarios) {
 $(document).ready(function () {
   readGoogleSheet('voluntarios', addVoluntarios);
   readGoogleSheet('eventos', addEventos);
+  readGoogleSheet('alunos', addAlunos);
 });
